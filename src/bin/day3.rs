ef2f1a5374406d849eb898eea99e583c8bd493f9
thatguydoru <jtorres.doru@gmail.com>
2023-12-07 14:29:28 +0800

    use regex for day3 as part in initializing cache

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -1,5 +1,7 @@
 use std::collections::HashMap;
-use std::str::FromStr;
+use std::rc::Rc;
+
+use regex::Regex;
 
 const _TEST_INPUT: &str = "467..114..
 ...*......
@@ -15,25 +17,13 @@ const INPUT: &str = include_str!("day3.in");
 
 fn main() {
     let input = INPUT;
-    let mut cache: HashMap<char, Vec<Vec<u32>>> = HashMap::new();
-    let line_len = input.lines().count(); // the input should be of size n x n
-
-    for (found, symbol) in symbol_indexes(input) {
-        let top = parse_line(&input[found - (line_len + 4)..=found - (line_len + 4) + 6]);
-        let mid = parse_line(&input[found - 3..=found + 3]);
-        let bot = parse_line(&input[found + (line_len + 4) - 6..=found + (line_len + 4)]);
-
-        cache
-            .entry(symbol)
-            .and_modify(|v| v.push(top.iter().chain(&mid).chain(&bot).cloned().collect()))
-            .or_insert(vec![top.iter().chain(&mid).chain(&bot).cloned().collect()]);
-    }
+    let cache = init_cache(input);
 
     println!("PART 1 ANSWER: {}", part_one_solution(&cache));
     println!("PART 2 ANSWER: {}", part_two_solution(&cache));
 }
 
-fn part_one_solution(cache: &HashMap<char, Vec<Vec<u32>>>) -> u32 {
+fn part_one_solution(cache: &HashMap<char, Vec<Rc<[u32]>>>) -> u32 {
     let mut sum = 0;
 
     for (_, part_numbers) in cache.iter() {
@@ -46,7 +36,7 @@ fn part_one_solution(cache: &HashMap<char, Vec<Vec<u32>>>) -> u32 {
     sum
 }
 
-fn part_two_solution(cache: &HashMap<char, Vec<Vec<u32>>>) -> u32 {
+fn part_two_solution(cache: &HashMap<char, Vec<Rc<[u32]>>>) -> u32 {
     let mut sum = 0;
     let gears = cache.get(&'*').unwrap();
 
@@ -61,89 +51,49 @@ fn part_two_solution(cache: &HashMap<char, Vec<Vec<u32>>>) -> u32 {
     sum
 }
 
-fn parse_line(line: &str) -> Vec<u32> {
-    let mut numbers = Vec::new();
-    let ascii = line.as_bytes();
-    let mid = line.len() / 2;
+fn init_cache(input: &str) -> HashMap<char, Vec<Rc<[u32]>>> {
+    let number_rgx = Regex::new(r"\d+").unwrap();
+    let symbol_rgx = Regex::new(r"[\D--\.\n]").unwrap();
+    let line_len = input.lines().count(); // input should be n x n
+    let mut cache: HashMap<char, Vec<Rc<[u32]>>> = HashMap::new();
 
-    if ascii[mid].is_ascii_digit() {
-        let left = ascii[mid - 1].is_ascii_digit();
-        let right = ascii[mid + 1].is_ascii_digit();
-        let num = match (left, right) {
-            (true, false) => parse_left(line, mid),
-            (false, true) => parse_right(line, mid),
-            (true, true) => parse_right(line, mid - 1),
-            (false, false) => parse_right(line, mid),
+    for symbol_match in symbol_rgx.find_iter(input) {
+        let symbol_idx = symbol_match.start();
+
+        /* NOTE:
+         * ....... top
+         * ...*... middle
+         * ....... bottom
+         * The assumption here is that the number of digits on each number ranges
+         * from 1 to 3. This assumption was due to the input's structure.
+         * */
+        let surrounding = [
+            &input[symbol_idx - (line_len + 4)..=symbol_idx - (line_len + 4) + 6],
+            &input[symbol_idx - 3..=symbol_idx + 3],
+            &input[symbol_idx + (line_len + 4) - 6..=symbol_idx + (line_len + 4)],
+        ];
+
+        let symbol = symbol_match.as_str().chars().next().unwrap();
+        let mut neighbors = Vec::new();
+
+        for line in surrounding {
+            for number_match in number_rgx.find_iter(line) {
+                let start = number_match.start();
+                let end = number_match.end();
+
+                if (2..=4).contains(&start) || (2..=4).contains(&(end - 1)) {
+                    neighbors.push(number_match.as_str().parse().unwrap());
+                }
+            }
         }
-        .unwrap();
 
-        numbers.push(num);
-    } else {
-        if let Ok(num) = parse_left(line, mid - 1) {
-            numbers.push(num);
-        }
+        let neighbors: Rc<[u32]> = neighbors.into();
 
-        if let Ok(num) = parse_right(line, mid + 1) {
-            numbers.push(num);
-        }
+        cache
+            .entry(symbol)
+            .and_modify(|v| v.push(neighbors.clone()))
+            .or_insert(vec![neighbors]);
     }
 
-    numbers
-}
-
-fn symbol_indexes(s: &str) -> impl Iterator<Item = (usize, char)> + '_ {
-    s.chars().enumerate().filter_map(|(idx, c)| {
-        if c != '.' && !c.is_ascii_digit() && !c.is_ascii_whitespace() {
-            Some((idx, c))
-        } else {
-            None
-        }
-    })
-}
-
-fn parse_left(s: &str, end: usize) -> Result<u32, <u32 as FromStr>::Err> {
-    let ascii = s.as_bytes();
-    let mut start = end;
-
-    while start > 0 && ascii[start - 1].is_ascii_digit() {
-        start -= 1;
-    }
-
-    s[start..=end].parse()
-}
-
-fn parse_right(s: &str, start: usize) -> Result<u32, <u32 as FromStr>::Err> {
-    let ascii = s.as_bytes();
-    let mut end = start;
-
-    while end < s.len() - 1 && ascii[end + 1].is_ascii_digit() {
-        end += 1;
-    }
-
-    s[start..=end].parse()
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_parse_left() {
-        let test_input = "123....";
-        let test_input1 = ".23....";
-        let mid = 3;
-
-        assert_eq!(parse_left(test_input, mid - 1), Ok(123));
-        assert_eq!(parse_left(test_input1, mid - 1), Ok(23));
-    }
-
-    #[test]
-    fn test_parse_right() {
-        let test_input = "....123";
-        let test_input1 = "....1..";
-        let mid = 3;
-
-        assert_eq!(parse_right(test_input, mid + 1), Ok(123));
-        assert_eq!(parse_right(test_input1, mid + 1), Ok(1));
-    }
+    cache
 }
